54d54b488ccd0a2fd55db101580f0907040bcfb8
ruchamahalkar <136054773+ruchamahalkar@users.noreply.github.com>
2023-06-11 22:48:32 +0530

    added query to get the top 3 most popular categories of products ordered

--- top3_popular_categories.sql
+++ top3_popular_categories.sql
@@ -2,4 +2,4 @@
 
 SELECT category_id, COUNT(order_id) AS total_orders FROM products p
 JOIN order_details od ON p.product_id=od.product_id
-GROUP BY category_id ORDER BY total_orders DESC LIMIT 3;
+GROUP BY category_id ORDER BY total_orders DESC LIMIT 3 ;
