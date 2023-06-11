fadb2bdb99daf7f1d9b7109d22bd513673848d58
ruchamahalkar <136054773+ruchamahalkar@users.noreply.github.com>
2023-06-11 22:57:24 +0530

    query added to get customers who have ordered products from all categories

--- customers_ordered_all_categories.sql
+++ customers_ordered_all_categories.sql
@@ -3,4 +3,4 @@
 SELECT customer_id FROM order_details od
 JOIN  orders o ON o.order_id = od.order_id
 JOIN products p ON p.product_id = od.product_id
-GROUP BY o.order_id HAVING COUNT (DISTINCT(category_id))>7;
+GROUP BY o.order_id HAVING COUNT (DISTINCT(category_id))>7 ;
