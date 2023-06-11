6bb8ca26b4f435d2e9d881cf86483cc1ca7d324a
ruchamahalkar <136054773+ruchamahalkar@users.noreply.github.com>
2023-06-11 22:59:03 +0530

    added query to get the total sales for each year

--- total_sales_per_year.sql
+++ total_sales_per_year.sql
@@ -1,4 +1,4 @@
 -- Find the total sales for each year
 SELECT  YEAR(order_date ) AS order_year , SUM(quantity*unit_price) AS sales  
 FROM order_details od JOIN orders o ON od.order_id = o.order_id
-GROUP BY YEAR(order_date);
+GROUP BY YEAR(order_date) ;
