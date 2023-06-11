940d3931b34967c098df01b2421dbc3eb69bbf3e
ruchamahalkar <136054773+ruchamahalkar@users.noreply.github.com>
2023-06-11 22:50:33 +0530

    added query to get the month in the year 2016 with the highest total sales

--- highest_sales_month.sql
+++ highest_sales_month.sql
@@ -2,4 +2,4 @@
 SELECT MONTH(order_date) AS month_order, SUM(quantity*unit_price) AS sales  
 FROM order_details od 
 JOIN orders o ON od.order_id = o.order_id WHERE YEAR(order_date)  IS 2016
-ORDER BY sales DESC LIMIT 1;
+ORDER BY sales DESC LIMIT 1 ;
