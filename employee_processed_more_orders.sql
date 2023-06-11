e42421431ecb8b549887b260a527dc2a249c578d
ruchamahalkar <136054773+ruchamahalkar@users.noreply.github.com>
2023-06-11 22:44:17 +0530

    added query to get employees who have processed more than 100 orders

--- employee_processed_more_orders.sql
+++ employee_processed_more_orders.sql
@@ -1,3 +1,3 @@
 -- Find the employees who have processed more than 100 orders
 
-SELECT  employee_id , COUNT(order_id) AS total_orders FROM orders GROUP BY employee_id HAVING total_orders>100  ;
+SELECT  employee_id , COUNT(order_id) AS total_orders FROM orders GROUP BY employee_id HAVING total_orders>100 ;
