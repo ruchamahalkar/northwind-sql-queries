cce432032a100bc61d880436477bd48b9ae8e117
ruchamahalkar <136054773+ruchamahalkar@users.noreply.github.com>
2023-06-11 22:55:57 +0530

    added query to get suppliers who supply the top 5 most sold products

--- suppliers_supply_top5_sold_products.sql
+++ suppliers_supply_top5_sold_products.sql
@@ -1,4 +1,4 @@
 -- Find the suppliers who supply the top 5 most sold products
 SELECT   supplier_id FROM order_details od
 JOIN products p ON p.product_id = od.product_id
-GROUP BY od.product_id ORDER BY quantity DESC LIMIT 5;
+GROUP BY od.product_id ORDER BY quantity DESC LIMIT 5  ;
