0a169829bdb59834d5d3b74ec47565915af092d2
ruchamahalkar <136054773+ruchamahalkar@users.noreply.github.com>
2023-06-11 22:52:20 +0530

    added query to get employee who processed the most orders in August 2016

--- employee_processed_most_orders.sql
+++ employee_processed_most_orders.sql
@@ -2,4 +2,4 @@
 
 SELECT  employee_id  , COUNT(order_id) AS total_orders FROM orders
 WHERE order_date BETWEEN '2016-08-01' AND '2016-08-31'
-GROUP BY employee_id  order by total_orders DESC LIMIT 1;
+GROUP BY employee_id  order by total_orders DESC LIMIT 1 ;
