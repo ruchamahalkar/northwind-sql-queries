45d04e83fdcddcce70bf7fdc58ccaaac4da9f8f1
ruchamahalkar <136054773+ruchamahalkar@users.noreply.github.com>
2023-06-11 22:45:59 +0530

    added query to get customer who has ordered the most ‘Chai’ product

--- customer_ordered_most_chai.sql
+++ customer_ordered_most_chai.sql
@@ -5,4 +5,4 @@ FROM order_details  od
 JOIN orders o ON o.order_id = od.order_id 
 JOIN products p ON od.product_id = p.product_id
 WHERE product_name = 'Chai'
-GROUP BY customer_id ORDER BY no_orders DESC LIMIT 1;
+GROUP BY customer_id ORDER BY no_orders DESC LIMIT 1 ;
