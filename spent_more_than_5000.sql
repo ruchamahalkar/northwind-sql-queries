fd5ece974af1399c1eb0fccfcfef09e251dae1c3
ruchamahalkar <136054773+ruchamahalkar@users.noreply.github.com>
2023-06-11 23:02:35 +0530

    added query to get customers who have spent more than $5000 in total

--- spent_more_than_5000.sql
+++ spent_more_than_5000.sql
@@ -1,15 +1,8 @@
-<<<<<<< HEAD
+
 -- Find the customers who have spent more than $5000 in total
 
 SELECT  customer_id,  SUM(unit_price* quantity )   AS sales 
 FROM order_details od
 JOIN orders o ON od.order_id = o.order_id
 GROUP BY customer_id HAVING sales> 5000;
-=======
--- Find the customers who have spent more than $5000 in total
 
-SELECT  customer_id,  SUM(unit_price* quantity )   AS sales 
-FROM order_details od
-JOIN orders o ON od.order_id = o.order_id
-GROUP BY customer_id HAVING sales> 5000;
->>>>>>> refs/remotes/origin/main
