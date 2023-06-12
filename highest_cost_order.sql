59b54f73013d6808c65631cadb74d446e06e6575
ruchamahalkar <136054773+ruchamahalkar@users.noreply.github.com>
2023-06-12 21:04:11 +0530

    added query to get the order with the highest total cost

--- highest_cost_order.sql
+++ highest_cost_order.sql
@@ -1,4 +1,4 @@
 -- Find the order with the highest total cost
 
-SELECT order_id , (unit_price* quantity) - (unit_price* quantity*discount) AS total_cost
+SELECT order_id ,sum( (unit_price* quantity) - (unit_price* quantity*discount)) AS total_cost
 FROM order_details ORDER BY total_cost desc LIMIT  1;
