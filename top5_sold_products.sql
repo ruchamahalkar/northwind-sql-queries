dbcc468104d488bd98d4479750fe7e660f0c6d24
ruchamahalkar <136054773+ruchamahalkar@users.noreply.github.com>
2023-06-12 21:00:29 +0530

    added query to get the top 5 most sold products

--- top5_sold_products.sql
+++ top5_sold_products.sql
@@ -1,6 +1,6 @@
 /*Get the top 5 most sold products*/
 
-SELECT product_id, quantity FROM order_details GROUP BY product_id ORDER BY quantity DESC LIMIT 5
+SELECT product_id, sum(quantity) FROM order_details GROUP BY product_id ORDER BY quantity DESC LIMIT 5
 
 
 
